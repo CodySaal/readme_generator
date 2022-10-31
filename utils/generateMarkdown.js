43c5b362b70a118792fe341d1637678933860439
Cody <110630101+CodySaal@users.noreply.github.com>
2022-10-31 11:59:44 -0500

    Adjusted readme template and adjusted link and badge format

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,31 +2,31 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === "Apache License 2.0") {
-    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]"
+    return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)"
   } else if (license === "GNU General Public License v3.0") {
-    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]"
+    return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)"
   } else if (license === "MIT License") {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]"
+    return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)"
   } else if (license === "BSD 2-Clause 'Simplified' License") {
-    return "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)]"
+    return "![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)"
   } else if (license === "BSD 3-Clause 'New' or 'Revised' License") {
-    return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]"
+    return "![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)"
   } else if (license === "Boost Software License 1.0") {
-    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]"
+    return "![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)"
   } else if (license === "Creative Commons Zero v1.0 Universal") {
-    return "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)]"
+    return "![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)"
   } else if (license === "Eclipse Public License 2.0") {
-    return "[![License: EPL V2](https://img.shields.io/badge/License-EPLv2-blue.svg)]"
+    return "![License: EPL V2](https://img.shields.io/badge/License-EPLv2-blue.svg)"
   } else if (license === "GNU Affero General Public License v3.0") {
-    return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)]"
+    return "![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)"
   } else if (license === "GNU General Public License v2.0") {
-    return "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)]"
+    return "![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)"
   } else if (license === "GNU Lesser General Public License v2.1") {
-    return "[![License: LGP v2](https://img.shields.io/badge/License-LGPV2-blue.svg)]"
+    return "![License: LGP v2](https://img.shields.io/badge/License-LGPV2-blue.svg)"
   } else if (license === "Mozilla Public License 2.0") {
-    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]"
+    return "![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)"
   } else if (license === "The Unlicense") {
-    return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]"
+    return "![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)"
   } else {
     return ""
   }
@@ -36,31 +36,31 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === "Apache License 2.0") {
-    return "(https://opensource.org/licenses/Apache-2.0)"
+    return "https://opensource.org/licenses/Apache-2.0"
   } else if (license === "GNU General Public License v3.0") {
-    return "(https://www.gnu.org/licenses/gpl-3.0)"
+    return "https://www.gnu.org/licenses/gpl-3.0"
   } else if (license === "MIT License") {
-    return "(https://opensource.org/licenses/MIT)"
+    return "https://opensource.org/licenses/MIT"
   } else if (license === "BSD 2-Clause 'Simplified' License") {
-    return "(https://opensource.org/licenses/BSD-2-Clause)"
+    return "https://opensource.org/licenses/BSD-2-Clause"
   } else if (license === "BSD 3-Clause 'New' or 'Revised' License") {
-    return "(https://opensource.org/licenses/BSD-3-Clause)"
+    return "https://opensource.org/licenses/BSD-3-Clause"
   } else if (license === "Boost Software License 1.0") {
-    return "(https://www.boost.org/LICENSE_1_0.txt)"
+    return "https://www.boost.org/LICENSE_1_0.txt"
   } else if (license === "Creative Commons Zero v1.0 Universal") {
-    return "(http://creativecommons.org/publicdomain/zero/1.0/)"
+    return "http://creativecommons.org/publicdomain/zero/1.0/"
   } else if (license === "Eclipse Public License 2.0") {
-    return "(https://opensource.org/licenses/EPL-2.0)"
+    return "https://opensource.org/licenses/EPL-2.0"
   } else if (license === "GNU Affero General Public License v3.0") {
-    return "(https://www.gnu.org/licenses/agpl-3.0)"
+    return "https://www.gnu.org/licenses/agpl-3.0"
   } else if (license === "GNU General Public License v2.0") {
-    return "(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
+    return "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"
   } else if (license === "GNU Lesser General Public License v2.1") {
-    return "(https://www.gnu.org/licenses/old-licenses/lgpl-2.0)"
+    return "https://www.gnu.org/licenses/old-licenses/lgpl-2.0"
   } else if (license === "Mozilla Public License 2.0") {
-    return "(https://opensource.org/licenses/MPL-2.0)"
+    return "https://opensource.org/licenses/MPL-2.0"
   } else if (license === "The Unlicense") {
-    return "(http://unlicense.org/)"
+    return "http://unlicense.org/"
   } else {
     return ""
   }
@@ -72,7 +72,7 @@ function renderLicenseSection(license) {
   if (license === "None") {
     return ""
   } else {
-    return `## License\n ${license}: ${renderLicenseLink(license)}`
+    return `## License\n  ${license}: ${renderLicenseLink(license)}`
   }
 }
 
@@ -92,18 +92,19 @@ function generateMarkdown(data) {
     email,
   } = data
   return `# ${title}
+  [${renderLicenseBadge(license)}](${renderLicenseLink(license)})
   ## Description
 
   ${description}
 
-  ## Table of Contents (Optional)
-
-  If your README is long, add a table of contents to make it easy for users to find what they need.
+  ## Table of Contents 
 
   - [Installation](#installation)
   - [Usage](#usage)
-  - [Credits](#credits)
   - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
 
   ## Installation
 
@@ -115,7 +116,7 @@ function generateMarkdown(data) {
 
   ${renderLicenseSection(license)}
 
-  ## How to Contribute
+  ## Contributing
 
   ${contribution}
 
@@ -124,9 +125,8 @@ function generateMarkdown(data) {
   ${testing}
 
   ## Questions
-
-  GitHub Profile: https://github.com/${github}
-
+[GitHub Profile](#https://github.com/${github})
+  
   I am also available via email at: ${email}
   `;
 }

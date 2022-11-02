f7f9d3a5e7b80522ca557bceb4f2f6599e89b34f
Cody <110630101+CodySaal@users.noreply.github.com>
2022-11-01 21:55:03 -0500

    Added comments

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
+// array of questions for user input
 const questions = [
     {
         type: "input",
@@ -53,14 +53,14 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
+// function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, err => {
         if (err) throw err
         console.log("README saved!")
     })
 }
-// TODO: Create a function to initialize app
+// function to initialize app
 function init() {
     inquirer
         .prompt(questions)
